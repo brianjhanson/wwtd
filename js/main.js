a1f590690288e1ca4dcc2ff83df60b26a420ee08
Brian Hanson <brian@brianhanson.net>
2015-05-31 21:53:17 -0500

    site

---
+++ js/main.js
@@ -0,0 +1,14 @@
+jQuery(function($){
+    
+  $.supersized({
+    slide_interval: 3000,
+    transition: 1,
+    transition_speed: 500,
+    slide_links: false,
+    slides: [
+      { image: "img/IMG_3545.JPG" },
+      { image: "img/IMG_3546.JPG" },
+      { image: "img/IMG_3547.JPG" }
+    ]
+  });
+});
